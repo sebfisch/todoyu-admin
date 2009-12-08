d1fa7af561fa854e7b2f3cc3dee98f7476b23ffe
kstenschke <kstenschke@7f067039-fbb2-4aac-a504-d491c780d6a3>
2009-12-08 14:55:19 +0000

    * * removed person color border from appointments in portal list view * replaced redundant panelWidget inner-information styles by one new class * fixed some not so successful german localizations

--- controller/TodoyuAdminExtActionController.class.php
+++ controller/TodoyuAdminExtActionController.class.php
@@ -1,12 +1,29 @@
 <?php
-
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2009 snowflake productions gmbh
+*  All rights reserved
+*
+*  This script is part of the todoyu project.
+*  The todoyu project is free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License, version 2,
+*  (http://www.gnu.org/licenses/old-licenses/gpl-2.0.html) as published by
+*  the Free Software Foundation;
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
 
 class TodoyuAdminExtActionController extends TodoyuActionController {
 
 	public function defaultAction(array $params) {
 		TodoyuFrontend::setActiveTab('todoyu');
 		TodoyuFrontend::setActiveSubmenuTab('todoyu', 'admin');
-
 
 		TodoyuPage::init('ext/admin/view/ext.tmpl');
 		TodoyuPage::setTitle('LLL:admin.page.title');
@@ -38,10 +55,11 @@ class TodoyuAdminExtActionController extends TodoyuActionController {
 	}
 
 
+
+
 	public function _unknownAction($actionName, array $params) {
 		return $this->defaultAction($params);
 	}
-
 
 }
 
