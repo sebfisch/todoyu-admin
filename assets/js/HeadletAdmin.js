446bfde364ebd772919776579887100661fd3f2c
ferni <ferni@7f067039-fbb2-4aac-a504-d491c780d6a3>
2010-03-12 14:22:46 +0000

    * Headlets * Added profile Headlet.js * Removed todoyu tab

--- assets/js/HeadletAdmin.js
+++ assets/js/HeadletAdmin.js
@@ -1,12 +1,13 @@
 Todoyu.Ext.admin.Headlet.Admin = {
 	
 	init: function() {
-		
+		if( document.location.href.toQueryParams().ext === 'admin' ) {
+			this.headlet.setActive('admin');
+		}
 	},
 	
 	onButtonClick: function(event) {	
 		Todoyu.goTo('admin', 'ext');
 	}
-	
 	
 };
--- config/page.php
+++ config/page.php
@@ -2,7 +2,6 @@
 
 	// Only admins can access the admin module
 if( TodoyuAuth::isAdmin() ) {
-	TodoyuFrontend::addSubmenuEntry('todoyu', 'admin', 'LLL:admin.tab.label', '?ext=admin', 1);
 	TodoyuHeadManager::addHeadlet('TodoyuHeadletAdmin', 20);
 }
 
