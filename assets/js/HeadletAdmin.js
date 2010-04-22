2581b92d2489150270ff01c116e1f9c5184ce8a5
ferni <ferni@7f067039-fbb2-4aac-a504-d491c780d6a3>
2010-04-22 09:20:19 +0000

    * Bugfixes

--- assets/js/HeadletAdmin.js
+++ assets/js/HeadletAdmin.js
@@ -2,7 +2,7 @@ Todoyu.Ext.admin.Headlet.Admin = {
 	
 	init: function() {
 		if( document.location.href.toQueryParams().ext === 'admin' ) {
-			this.headlet.setActive('admin');
+			//this.headlet.setActive('admin');
 		}
 	},
 	
