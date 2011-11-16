95a96126c3c4c1931bfd28649cdd270f071c716c
nkarrer <nkarrer@7f067039-fbb2-4aac-a504-d491c780d6a3>
2011-11-16 08:04:02 +0000

    * fixed unit tests

--- test/phpunit/TodoyuAdminManagerTest.class.php
+++ test/phpunit/TodoyuAdminManagerTest.class.php
@@ -101,7 +101,7 @@ class TodoyuAdminManagerTest extends PHPUnit_Framework_TestCase {
 
 			// Assert modules present at all
 		$expected	= 'array';
-		$this->assertInternalType($expected, $activeModules);
+		$this->assertType($expected, $activeModules);
 
 			// Assert at least 5 modules (of sysmanager, which unittest is one of) present
 		$amountModules	= sizeof($activeModules);
@@ -119,7 +119,7 @@ class TodoyuAdminManagerTest extends PHPUnit_Framework_TestCase {
 			// Assert config of each module to contain: key, label and render callback
 		foreach($activeModules as $activeModule) {
 			$expected	= 'array';
-			$this->assertInternalType($expected, $activeModule);
+			$this->assertType($expected, $activeModule);
 
 			$this->assertArrayHasKey('key', $activeModule);
 			$this->assertArrayHasKey('label', $activeModule);
